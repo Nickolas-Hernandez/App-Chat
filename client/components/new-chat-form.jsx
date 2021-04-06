ded246524065dec668c09cb7288e2cacdd06ca7b
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-06 18:43:08 +0000

    Added background overlay and h2 element to form. Also styled elements. #2

--- client/components/new-chat-form.jsx
+++ client/components/new-chat-form.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export default class newChatForm extends React.Component {
+export default class NewChatForm extends React.Component {
 
+  render() {
+    return (
+      <>
+        <div className="overlay"></div>
+        <form className="new-chat-form">
+          <h2>Create</h2>
+        </form>
+      </>
+    );
+  }
 }
