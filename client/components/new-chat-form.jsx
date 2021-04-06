b8a597c7a64d404c29cc7ae28ed457ffeef91b5e
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-06 19:43:26 +0000

    added input elements to form. #2

--- client/components/new-chat-form.jsx
+++ client/components/new-chat-form.jsx
@@ -8,6 +8,11 @@ export default class NewChatForm extends React.Component {
         <div className="overlay">
           <form className="new-chat-form">
             <h3 className="form-tab active">Create</h3>
+            <label htmlFor="chat-name">Enter chat name</label>
+            <input type="text" name="chat-name" id="chat-name"/>
+            <label htmlFor="users-name">Enter your name</label>
+            <input type="text" name="users-name" id="users-name"/>
+            <input type="submit" value="Submit"/>
           </form>
         </div>
       </>
