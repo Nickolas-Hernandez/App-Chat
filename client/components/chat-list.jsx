e81221d3258bc4de3055802c735695de2137ca09
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-08 23:58:42 +0000

    Added event listner to icon and defined a function to handle opening form. #1

--- client/components/chat-list.jsx
+++ client/components/chat-list.jsx
@@ -12,7 +12,9 @@ export default class ChatList extends React.Component {
         <div className="chat-list-header">
           <div className="wrapper">
             <h1>Chats</h1>
-            <i className="fas fa-plus plus-icon"></i>
+            <i
+            onClick={this.openNewChatForm}
+            className="fas fa-plus plus-icon"></i>
           </div>
         </div>
         <ul className="chat-list"></ul>
