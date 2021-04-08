8d2271ee0a023395d2f46ae502f294bd9b8209ee
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-08 03:29:45 +0000

    Created socket and figured out emit and eventListening. #2

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import Home from './pages/home';
+import { io } from 'socket.io-client';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.socket = null;
+  }
+
+  componentDidMount() {
+    this.socket = io();
+    console.log(this.socket);
+    this.socket.emit('newRoom', { room: 'room bitch' });
+  }
+
   render() {
     return <Home />;
   }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,3 @@
-const httpServer = require('http').createServer();
-const io = require('socket.io')(httpServer);
-
-console.log('server here!!!', io);
-
 require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
@@ -11,7 +6,18 @@ const app = express();
 
 app.use(staticMiddleware);
 
-app.listen(process.env.PORT, () => {
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  console.log('connected!');
+  console.log('ROOOOOOOOOMS ! ! ! !', socket.rooms);
+  socket.on('newRoom', room => {
+    console.log('room', room);
+  });
+});
+
+server.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`express server listening on port ${process.env.PORT}`);
+  console.log(`http server listening on port ${process.env.PORT}`);
 });
