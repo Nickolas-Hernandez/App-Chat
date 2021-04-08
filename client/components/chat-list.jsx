aa9169a080aae27e7413704fbfdba9e0e682aee3
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-08 22:55:59 +0000

    Created div element for chat list header. #1

--- client/components/chat-list.jsx
+++ client/components/chat-list.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 export default class ChatList extends React.Component {
   render() {
     return (
+      <>
+        <div className="chat-list-header">
 
+        </div>
+      </>
     );
   }
 }
