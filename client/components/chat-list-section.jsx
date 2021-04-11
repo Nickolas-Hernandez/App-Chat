5e8dc5e1d1431a979cbec14c709a62853ca84dc6
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-11 03:11:20 +0000

    Put fetch request for chat rooms into its own function so it could be called multiple times. Also refactored code so resetState method returns a blank state that can be reused in different functions. #1

--- client/components/chat-list-section.jsx
+++ client/components/chat-list-section.jsx
@@ -21,35 +21,26 @@ export default class ChatListSection extends React.Component {
   }
 
   componentDidMount() {
-    fetch('/api/chatRooms')
-      .then(response => response.json())
-      .then(result => {
-        const rooms = { chatRooms: result };
-        const formInfo = { form: Object.assign({}, this.state.form) };
-        const newState = Object.assign({}, this.state, formInfo, rooms);
-        this.setState(newState);
-      })
-      .catch(err => console.error(err));
+    this.getAllChatRooms();
   }
 
   openNewChatForm() {
-    const openForm = Object.assign({}, this.state);
-    openForm.formIsOpen = true;
-    this.setState(openForm);
+    const newState = this.resetState();
+    newState.formIsOpen = true;
+    this.setState(newState);
   }
 
   handleChange(target) {
-    let newState;
-    const rooms = { chatRooms: this.state.chatRooms.slice() };
+    const buildState = this.resetState();
     const form = { form: Object.assign({}, this.state.form) };
+    const newState = Object.assign({}, buildState, form);
+    newState.formIsOpen = true;
     if (target.id === 'chat-name') {
-      form.form.chatName = target.value;
-      newState = Object.assign({}, this.state, form, rooms);
+      newState.form.chatName = target.value;
       this.setState(newState);
       return;
     }
-    form.form.userName = target.value;
-    newState = Object.assign({}, this.state, form, rooms);
+    newState.form.userName = target.value;
     this.setState(newState);
   }
 
@@ -62,23 +53,34 @@ export default class ChatListSection extends React.Component {
     fetch('/api/newRoom', init)
       .then(response => response.json())
       .then(result => {
-        this.resetState(result);
+        this.getAllChatRooms();
       })
       .catch(err => console.error(err));
   }
 
   closeForm(event) {
     if (event.target.className !== 'overlay') return;
-    this.resetState();
+    const newState = this.resetState();
+    this.setState(newState);
   }
 
-  resetState(newRoom) {
+  resetState() {
     const rooms = { chatRooms: this.state.chatRooms.slice() };
-    if (newRoom) rooms.chatRooms.unshift(newRoom);
     const formInfo = { form: { chatName: '', userName: '' } };
     const openForm = { formIsOpen: false };
     const newState = Object.assign({}, this.state, openForm, formInfo, rooms);
-    this.setState(newState);
+    return newState;
+  }
+
+  getAllChatRooms() {
+    fetch('/api/chatRooms')
+      .then(response => response.json())
+      .then(result => {
+        const newState = this.resetState();
+        newState.chatRooms = result;
+        this.setState(newState);
+      })
+      .catch(err => console.error(err));
   }
 
   render() {
