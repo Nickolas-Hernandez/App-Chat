df0fb8ebddb9383cf7381ed7112343948038a166
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-12 21:46:07 +0000

    wrapped message area elements in a div and gave elements more defined class names. Also styled elements in styles.css. #3

--- client/components/message-area.jsx
+++ client/components/message-area.jsx
@@ -4,12 +4,15 @@ export default class MessageArea extends React.Component {
 
   render() {
     return (
-      <div className="message-area-header">
-        <div className="wrapper">
-          <i className="fas fa-angle-left back-arrow"></i>
-          <h1>ChatRoom</h1>
-          <i className="fas fa-sign details-icon"></i>
+      <div className="message-area">
+        <div className="message-area-header">
+          <div className="wrapper">
+            <i className="fas fa-angle-left back-arrow"></i>
+            <h1>ChatRoom</h1>
+            <i className="fas fa-sign details-icon"></i>
+          </div>
         </div>
+        <div className="messages-view"></div>
       </div>
     );
   }
