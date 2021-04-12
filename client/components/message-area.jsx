652241c322a85ef154b87328108fb5e2cbdca334
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-12 20:03:36 +0000

    imported message area component into pages/home.jsx. #3

--- client/components/message-area.jsx
+++ client/components/message-area.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default class MessageArea extends React.Component {
+  render() {
 
+  }
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ChatListSection from '../components/chat-list-section';
+import MessageArea from '../components/message-area';
 
 export default function Home(props) {
   return (
     <>
-      <ChatListSection />
+      {/* <ChatListSection /> */}
+      <MessageArea />
     </>
   );
 }
