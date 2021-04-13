5c7624e13a88e38c842486752dd992adf2af4d79
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-13 17:45:55 +0000

    Adjusted styling for text area of message input. #3

--- client/components/message-area.jsx
+++ client/components/message-area.jsx
@@ -7,9 +7,11 @@ export default class MessageArea extends React.Component {
   }
 
   adjustTextareaHeight(event) {
+    // console.log(event);
     event.target.style.height = '24px';
     event.target.style.height = `${event.target.scrollHeight}px`;
-
+    event.target.parentElement.style.height = '36px';
+    event.target.parentElement.style.height = `${event.target.scrollHeight + 8}px`;
   }
 
   render() {
