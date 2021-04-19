8d6bf33fcc145d19494dba62fee6b39c396ebfd4
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-19 01:16:08 +0000

    Implements app drawer in mobile. #@4

--- client/components/chat-details-drawer.jsx
+++ client/components/chat-details-drawer.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
-export default function ChatDetailsDrawer(props) {
-  return (
+export default class ChatDetailsDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { drawerIsOpen: false };
+    this.openDrawer = this.openDrawer.bind(this);
+  }
+
+  openDrawer(event) {
+    if (!this.state.drawerIsOpen) {
+      this.setState({ drawerIsOpen: true });
+      return;
+    }
+    this.setState({ drawerIsOpen: false });
+  }
+
+  render() {
+    const { drawerIsOpen } = this.state;
+    console.log(drawerIsOpen);
+    return (
       <>
-        <i className="fas fa-sign details-icon"></i>
-        <div className="overlay details-drawer"></div>
-          <div className="chat-details-drawer">
+        <i onClick={this.openDrawer} className="fas fa-sign details-icon"></i>
+        <div className={drawerIsOpen ? 'overlay details-drawer ' : 'hidden' }></div>
+          <div className={drawerIsOpen ? 'chat-details-drawer active' : 'chat-details-drawer'}>
             <h3>Chat Room ID:</h3>
-            <p>{props.id}</p>
+            <p>{this.props.id}</p>
           </div>
       </>
-  );
+    );
+  }
 }
