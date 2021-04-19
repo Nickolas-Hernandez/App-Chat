5ac6a0106e007137dceaab8631967ac0f1a218e3
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-19 00:03:54 +0000

    Created ChatDetailsDrawer component. #4

---
+++ client/components/chat-details-drawer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ChatDetailsDrawer extends React.Component {
+  render() {
+    return (
+      <div className="chat-details-drawer">
+        <h3>Chat Room ID:</h3>
+        <p></p>
+      </div>
+    );
+  }
+}
